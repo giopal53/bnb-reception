d68d58c4e8e81ea0c217d84d45f2b5779d0153b6
giopal53 <giovanni@sigmastudio.it>
2025-12-12 22:15:01 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -13,6 +13,10 @@ async function sendTelegram(text) {
     text: text
   });
 }
+app.post("/webhook", async (req, res) => {
+  console.log("WEBHOOK RICEVUTO:", JSON.stringify(req.body));
+  res.status(200).json({ ok: true });
+});
 
 app.post("/webhook", async (req, res) => {
   const data = req.body;
